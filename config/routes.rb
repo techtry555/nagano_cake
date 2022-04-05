03a7419207f7d01cd7f9fcb17e803858a13fb25a
techtry555 <techtry555@gmail.com>
2022-04-05 12:59:06 +0000

    [Add]publicのコントローラ追加済

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,32 @@
 Rails.application.routes.draw do
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
 
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  ### 後でonlyで減らす
   namespace :admin do
     resources :items
     resources :homes
     resources :customers
     resources :orders
     resources :order_details
+  end
+
+  namespace :public do
+    resources :items
+    resources :homes
+    resources :customers
+    resources :orders
+    resources :addresses
+    resources :cart_items
   end
 end
