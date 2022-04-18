039fbe4a8523cedbc81dd722f83d51543e22b415
techtry555 <techtry555@gmail.com>
2022-04-18 12:51:21 +0000

    is_deletedカラムの情報追加、済み

--- db/migrate/20220405071804_devise_create_customers.rb
+++ db/migrate/20220405071804_devise_create_customers.rb
@@ -40,7 +40,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       t.string :postal_code
       t.string :address
       t.string :telephone_number
-      t.boolean :is_deleted
+      t.boolean :is_deleted, null: false, default: false #登録直後はfalse設定
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 2022_04_07_112317) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
