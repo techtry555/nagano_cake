c37992b3c7aa9e94ff3d5fdb260152b0a3cc2e02
techtry555 <techtry555@gmail.com>
2022-04-19 14:31:49 +0000

    管理者ログアウト実装、途中まで

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,6 +2,25 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+    after_sign_out_path_for(resource)
+  end
+
+  protected
+  ## deviseのメソッド
+  def after_sign_in_path_for(resource)
+    admin_path # 管理者top/注文履歴一覧画面
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 
   # GET /resource/sign_in
   # def new
