c05f1db16d348aeae19dc09dc746ed00d3b54005
techtry555 <techtry555@gmail.com>
2022-04-19 14:40:42 +0000

    管理者ログアウト完

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,15 +2,21 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
   def new
+     super
   end
 
-  def create
-  end
+  # POST /resource/sign_in
+   def create
+     super
+   end
 
-  def destroy
-    after_sign_out_path_for(resource)
-  end
+  # DELETE /resource/sign_out
+   def destroy
+     super
+   end
 
   protected
   ## deviseのメソッド
@@ -21,21 +27,6 @@ class Admin::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     new_admin_session_path
   end
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
 
   # protected
 
