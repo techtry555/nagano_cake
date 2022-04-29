5bec2bf52af42e7baa26588203dbcb02f5c7994c
techtry555 <techtry555@gmail.com>
2022-04-29 14:07:30 +0000

    管理画面に戻って修正開始

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,8 +5,14 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def unsubscribe
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'public/homes#top'
-  get '/about', to: 'public/homes#about'
-  get '/customers/my_page', to: 'public/customers#show'
-  get '/customers/edit', to: 'public/customers#edit'
-  get '/customers/unsubscribe', to: 'public/customers#unsubscribe'
-  get '/admin', to: 'admin/homes#top'
+  get 'about', to: 'public/homes#about'
+  get 'customers/my_page', to: 'public/customers#show'
+  get 'customers/edit', to: 'public/customers#edit'
+  get 'customers/unsubscribe', to: 'public/customers#unsubscribe'
+  get 'admin', to: 'admin/homes#top'
+  post 'customers', to: 'public/customers#update'
 
   # 管理者用 ## URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
